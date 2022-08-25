db19e7768cead49c7f410fb48dfce31ed42d45df
Dario Salvi <dariosalvi78@gmail.com>
2022-08-25 08:54:53 +0200

    adding comments on i18n

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -2,6 +2,10 @@ import { boot } from 'quasar/wrappers'
 import { createI18n } from 'vue-i18n'
 import messages from 'src/i18n'
 
+// read about 18n
+// https://blog.flycode.com/step-by-step-how-to-create-a-vue-multi-language-app-with-vue-i18n
+// https://quasar.dev/options/app-internationalization
+
 export const i18n = createI18n({
   locale: 'en',
   fallbackLocale: 'en',
