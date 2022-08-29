edfcb1792c5d34ed48b76e16a981a45bb9544cce
Dario Salvi <dariosalvi78@gmail.com>
2022-08-29 15:36:26 +0200

    removed vuelidate from email tester

--- src/components/admin/EmailTesterCard.vue
+++ src/components/admin/EmailTesterCard.vue
@@ -1,92 +1,79 @@
 <template>
   <!-- <q-page class="q-pa-lg"> -->
   <q-card>
-    <q-card-section>
-      <div class="text-h5">Send email from server</div>
-    </q-card-section>
-    <q-card-section>
-      <div class="row q-ma-sm">
-        <div class="col-2 text-bold">Email address: </div>
-        <div class="col">
-          <q-input
-            type="email"
-            v-model="emailAddress"
-            :error="vuelidate.emailAddress.$error"
-            error-message="An valid email address is required"
-            @blur="vuelidate.emailAddress.$touch"
-          />
+    <q-form ref="emailTesterForm">
+      <q-card-section>
+        <div class="text-h5">Send email from server</div>
+      </q-card-section>
+      <q-card-section>
+        <div class="row q-ma-sm">
+          <div class="col-2 text-bold">Email address: </div>
+          <div class="col">
+            <q-input
+              type="email"
+              v-model="emailAddress"
+              :rules="emailRules"
+            />
+          </div>
         </div>
-      </div>
-      <div class="row q-ma-sm">
-        <div class="col-2 text-bold">Subject: </div>
-        <div class="col">
-          <q-input
-            type="text"
-            v-model="emailSubject"
-            :error="vuelidate.emailSubject.$error"
-            error-message="An subject is required"
-            @blur="vuelidate.emailSubject.$touch"
-          />
+        <div class="row q-ma-sm">
+          <div class="col-2 text-bold">Subject: </div>
+          <div class="col">
+            <q-input
+              type="text"
+              v-model="emailSubject"
+              :rules="[val => !!val || 'Field is required']"
+            />
+          </div>
         </div>
-      </div>
-      <div class="row q-ma-sm">
-        <div class="col-2 text-bold"> Content: </div>
-        <div class="col">
-          <q-input
-            type="textarea"
-            rows="3"
-            v-model="emailContent"
-            :error="vuelidate.emailContent.$error"
-            error-message="Some content is required"
-            @blur="vuelidate.emailContent.$touch"
-          />
+        <div class="row q-ma-sm">
+          <div class="col-2 text-bold"> Content: </div>
+          <div class="col">
+            <q-input
+              type="textarea"
+              rows="3"
+              v-model="emailContent"
+              :rules="[val => !!val || 'Field is required']"
+            />
+          </div>
         </div>
-      </div>
-    </q-card-section>
-    <q-card-actions>
-      <q-btn
-        label="Send"
-        color="primary"
-        :loading="sending"
-        @click="sendEmail"
-      />
-    </q-card-actions>
+      </q-card-section>
+      <q-card-actions>
+        <q-btn
+          label="Send"
+          color="primary"
+          :loading="sending"
+          @click="sendEmail"
+        />
+      </q-card-actions>
+    </q-form>
   </q-card>
 </template>
 
 <script>
 import API from '@shared/API'
-import useVuelidate from '@vuelidate/core'
-import { required, email } from '@vuelidate/validators'
-
+import { testPattern } from 'quasar/src/utils/patterns'
 export default {
   name: 'EmailTesterCard',
-  setup () {
-    return { vuelidate: useVuelidate() }
-  },
   data () {
     return {
       emailAddress: '',
       emailSubject: '',
       emailContent: '',
-      sending: false
+      sending: false,
+      emailRules: [
+        val => !!val || 'Filed is required',
+        val => testPattern.email(val) || 'Must be an email address'
+      ]
     }
-  },
-  validations: {
-    emailAddress: { required, email },
-    emailSubject: { required },
-    emailContent: { required }
   },
   methods: {
     async sendEmail () {
-      this.vuelidate.emailAddress.$touch()
-      this.vuelidate.emailContent.$touch()
-      this.vuelidate.emailSubject.$touch()
-      if (this.vuelidate.emailAddress.$error || this.vuelidate.emailSubject.$error || this.vuelidate.emailContent.$error) {
-        this.$q.notify('Please correct the indicated fields.')
+      const valid = await this.$refs.emailTesterForm.validate(true)
+      if (!valid) {
+        this.$q.notify('Please correct the highlighted fields before submitting')
       } else {
         this.sending = true
-
         try {
           await API.sendTestEmail(this.emailAddress, this.emailSubject, this.emailContent)
           this.$q.notify({
