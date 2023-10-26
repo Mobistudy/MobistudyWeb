c1a0315892c08649aaa2ff04390b989589f4700d
Lukas Jönsson <94693238+LukasJnsson@users.noreply.github.com>
2023-10-26 13:01:31 +0200

    KAN-16 Created 'initializeFingerTappingDelayChart'
    Created finger tapping delay chart that display the delay between each tap in a line chart

--- src/components/researcher/taskvisualizations/FingerTappingDrawingVisualization.vue
+++ src/components/researcher/taskvisualizations/FingerTappingDrawingVisualization.vue
@@ -4,6 +4,9 @@
     <p>Average time left: {{ getAverageLeft() }}ms </p>
     <p>Average time right: {{ getAverageRight() }}ms</p>
   </div>
+  <div>
+    <canvas id="fingerTappingDelayChart"></canvas>
+  </div>
 </template>
 
 <script>
@@ -12,10 +15,11 @@ import { Chart } from 'chart.js/auto'
 export default {
   props: ['data'],
   mounted () {
-    this.initializeChart()
+    this.initializeFingerTappingChart()
+    this.initializeFingerTappingDelayChart()
   },
   methods: {
-    initializeChart () {
+    initializeFingerTappingChart () {
       const ctx = document.getElementById('fingerTappingChart').getContext('2d')
       const config = {
         type: 'scatter',
@@ -76,15 +80,73 @@ export default {
             },
             title: {
               display: true,
-              text: 'Finger Tapping Comparison'
+              text: 'Finger Tapping'
             }
           }
         }
       }
-
       this.chart = new Chart(ctx, config)
     },
 
+    initializeFingerTappingDelayChart () {
+      const ctx = document.getElementById('fingerTappingDelayChart').getContext('2d')
+      const config = {
+        type: 'scatter',
+        data: {
+          datasets: [
+            {
+              type: 'line',
+              label: 'Time Delay',
+              data: this.getTimeDelays(),
+              borderColor: 'gray',
+              fill: false,
+              borderWidth: 1,
+              pointRadius: 0
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              type: 'linear',
+              position: 'bottom',
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Milliseconds'
+              }
+            },
+            y: {
+              type: 'linear',
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Time Delay'
+              }
+            }
+          },
+          plugins: {
+            title: {
+              display: true,
+              text: 'Finger Tapping'
+            }
+          }
+        }
+      }
+      this.chart = new Chart(ctx, config)
+    },
+    getTimeDelays () {
+      /* eslint-disable-next-line */
+      const sortedData = this.data.sort((a, b) => a.msFromStart - b.msFromStart)
+      const timeDelays = []
+
+      for (let i = 1; i < sortedData.length; i++) {
+        const delay = sortedData[i].msFromStart - sortedData[i - 1].msFromStart
+        timeDelays.push({ x: sortedData[i].msFromStart, y: delay })
+      }
+      return timeDelays
+    },
     getLeft () {
       const leftTaps = this.data.filter(item => item.side === 'left')
       return leftTaps.map(item => ({
