c0ace366b92670baec9b9afd01340ead1667a399
Lukas Jönsson <94693238+LukasJnsson@users.noreply.github.com>
2023-11-07 10:28:02 +0100

    KAN-42 Conditional render on tugt charts

--- src/components/researcher/taskvisualizations/TugtVisualization.vue
+++ src/components/researcher/taskvisualizations/TugtVisualization.vue
@@ -3,14 +3,14 @@
     <p class="taskVisualizationHeader">Completed: {{ completed }}</p>
   </div>
   <div>
-    <q-toggle v-model="isFTCombined">{{ isFTCombined ? 'Module' : 'XYZ' }}</q-toggle>
-    <canvas id="tugtVectorChart"></canvas>
+    <q-toggle @click="handleFTToggleChange()" v-model="isACCombined">{{ isACCombined ? 'Module' : 'XYZ' }}</q-toggle>
+    <canvas id="tugtChart"></canvas>
   </div>
     <div class="resetChart">
-      <q-btn @click="tugtVectorChart.resetZoom()" class="reset_btn">Reset Zoom</q-btn>
+      <q-btn @click="tugtChart.resetZoom()" class="reset_btn">Reset Zoom</q-btn>
     </div>
   <div>
-    <q-toggle v-model="isITTCombined">{{ isITTCombined ? 'Module' : 'XYZ' }}</q-toggle>
+    <q-toggle @click="handleRoterToggleChange()" v-model="isRCombined">{{ isRCombined ? 'Module' : 'XYZ' }}</q-toggle>
     <canvas id="tugtRotarChart"></canvas>
   </div>
     <div class="resetChart">
@@ -27,19 +27,36 @@ Chart.register(zoomPlugin)
 export default {
   props: ['data', 'completed'],
   mounted () {
-    this.initializeRotarChart()
+    this.initializeRotarCombinedChart()
     this.initializeVectorChart()
-    console.log(this.getAccG('x'))
   },
   data () {
     return {
-      isFTCombined: ref(true),
-      isITTCombined: ref(true)
+      isACCombined: ref(true),
+      isRCombined: ref(true)
     }
   },
   methods: {
+    handleFTToggleChange () {
+      if (this.isACCombined) {
+        this.tugtChart.destroy()
+        this.initializeVectorChart()
+      } else {
+        this.tugtChart.destroy()
+        this.initializeXYZChart()
+      }
+    },
+    handleRoterToggleChange () {
+      if (this.isRCombined) {
+        this.tugtRotarChart.destroy()
+        this.initializeRotarCombinedChart()
+      } else {
+        this.tugtRotarChart.destroy()
+        this.initializeRotarSeparate()
+      }
+    },
     initializeVectorChart () {
-      const ctx = document.getElementById('tugtVectorChart').getContext('2d')
+      const ctx = document.getElementById('tugtChart').getContext('2d')
       const config = {
         type: 'line',
         data: {
@@ -56,7 +73,6 @@ export default {
           ]
         },
         options: {
-
           responsive: true,
           scales: {
             x: {
@@ -110,9 +126,102 @@ export default {
           }
         }
       }
-      this.tugtVectorChart = new Chart(ctx, config)
+      this.tugtChart = new Chart(ctx, config)
     },
-    initializeRotarChart () {
+    initializeXYZChart () {
+      const ctx = document.getElementById('tugtChart').getContext('2d')
+      const config = {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              type: 'line',
+              data: this.getAccG('x'),
+              borderColor: 'blue',
+              backgroundColor: 'blue',
+              fill: false,
+              borderWidth: 1,
+              pointRadius: 0,
+              label: 'Acc X'
+            },
+            {
+              type: 'line',
+              data: this.getAccG('y'),
+              borderColor: 'red',
+              backgroundColor: 'red',
+              fill: false,
+              borderWidth: 1,
+              pointRadius: 0,
+              label: 'Acc Y'
+            },
+            {
+              type: 'line',
+              data: this.getAccG('z'),
+              borderColor: 'green',
+              backgroundColor: 'green',
+              fill: false,
+              borderWidth: 1,
+              pointRadius: 0,
+              label: 'Acc Z'
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              type: 'linear',
+              position: 'bottom',
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Milliseconds',
+                color: '#459399'
+              }
+            },
+            y: {
+              ticks: {
+                stepSize: 0.2
+              },
+              suggestedMin: 0.1,
+              suggestedMax: 1,
+              type: 'linear',
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Acceleration Gravity',
+                color: '#459399'
+              }
+            }
+          },
+          plugins: {
+            legend: {
+            },
+            title: {
+              display: true,
+              text: 'Acceleration Gravity',
+              color: '#459399',
+              font: {
+                size: 16
+              }
+            },
+            zoom: {
+              zoom: {
+                wheel: {
+                  enabled: true
+                },
+                pinch: {
+                  enabled: true
+                },
+                mode: 'x'
+              }
+            }
+          }
+        }
+      }
+      this.tugtChart = new Chart(ctx, config)
+    },
+    initializeRotarCombinedChart () {
       const ctx = document.getElementById('tugtRotarChart').getContext('2d')
       const config = {
         type: 'line',
@@ -185,6 +294,96 @@ export default {
       }
       this.tugtRotarChart = new Chart(ctx, config)
     },
+    initializeRotarSeparate () {
+      const ctx = document.getElementById('tugtRotarChart').getContext('2d')
+      const config = {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              data: this.getAlphaBetaGamma('alpha'),
+              borderColor: 'blue',
+              backgroundColor: 'blue',
+              fill: false,
+              borderWidth: 1,
+              pointRadius: 0,
+              label: 'Alpha'
+            },
+            {
+              data: this.getAlphaBetaGamma('beta'),
+              borderColor: 'red',
+              backgroundColor: 'red',
+              fill: false,
+              borderWidth: 1,
+              pointRadius: 0,
+              label: 'Beta'
+            },
+            {
+              data: this.getAlphaBetaGamma('gamma'),
+              borderColor: 'green',
+              backgroundColor: 'green',
+              fill: false,
+              borderWidth: 1,
+              pointRadius: 0,
+              label: 'Gamma'
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              type: 'linear',
+              position: 'bottom',
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Milliseconds',
+                color: '#459399'
+              }
+            },
+            y: {
+              ticks: {
+                stepSize: 0.2
+              },
+              suggestedMin: 0.1,
+              suggestedMax: 1,
+              type: 'linear',
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Rotation',
+                color: '#459399'
+              }
+            }
+          },
+          plugins: {
+            legend: {
+            },
+            title: {
+              display: true,
+              text: 'Rotation',
+              color: '#459399',
+              font: {
+                size: 16
+              }
+            },
+            zoom: {
+              zoom: {
+                wheel: {
+                  enabled: true
+                },
+                pinch: {
+                  enabled: true
+                },
+                mode: 'x'
+              }
+            }
+          }
+        }
+      }
+      this.tugtRotarChart = new Chart(ctx, config)
+    },
     getMotionObjects () {
       return JSON.parse(JSON.stringify(this.data.motion))
     },
@@ -212,6 +411,17 @@ export default {
         })
       }
       return vectors
+    },
+    getAlphaBetaGamma (roter) {
+      const alphaBetaGamma = []
+      const roterArr = JSON.parse(JSON.stringify(this.data.motion.map(obj => obj.rotRate)))
+      for (let i = 0; i < roterArr.length; i++) {
+        alphaBetaGamma.push({
+          x: i,
+          y: Math.sqrt((roterArr[i][roter] * roterArr[i][roter]))
+        })
+      }
+      return alphaBetaGamma
     },
     getRoter () {
       const vectors = []
