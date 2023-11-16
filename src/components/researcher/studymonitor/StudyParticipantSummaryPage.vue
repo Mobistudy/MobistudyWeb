08d4f7b0a448c8d3f521d50560abd5c9faae219f
Lukas Jönsson <94693238+LukasJnsson@users.noreply.github.com>
2023-11-16 09:27:03 +0100

    KAN-45 Created visualization component for miband

--- src/components/researcher/studymonitor/StudyParticipantSummaryPage.vue
+++ src/components/researcher/studymonitor/StudyParticipantSummaryPage.vue
@@ -91,6 +91,9 @@
                   <div v-if="taskDataType === 'position'">
                     <PositionVisualization :data="taskDataContent" :completed="niceTimestamp(taskCompletedDate)" />
                   </div>
+                  <div v-if="taskDataType === 'miband'">
+                    <MibandVisualization :data="taskDataContent" :completed="niceTimestamp(taskCompletedDate)" />
+                  </div>
                 </q-card-section>
               </q-card>
             </q-dialog>
@@ -144,6 +147,7 @@ import TugtVisualization from '../taskvisualizations/TugtVisualization.vue'
 import VocalizationVisualization from '../taskvisualizations/VocalizationVisualization.vue'
 import PeakFlowVisualization from '../taskvisualizations/PeakFlowVisualization.vue'
 import PositionVisualization from '../taskvisualizations/PositionVisualization.vue'
+import MibandVisualization from '../taskvisualizations/MibandVisualization.vue'
 
 export default {
   name: 'StudyParticipant',
@@ -156,7 +160,8 @@ export default {
     TugtVisualization,
     VocalizationVisualization,
     PeakFlowVisualization,
-    PositionVisualization
+    PositionVisualization,
+    MibandVisualization
   },
   data () {
     return {
