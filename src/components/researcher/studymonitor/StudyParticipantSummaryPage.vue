ed98316dbc5871580334f31319b2729477118e64
Lukas Jönsson <94693238+LukasJnsson@users.noreply.github.com>
2023-11-22 10:07:53 +0100

    KAN-51 Added method that format task name

--- src/components/researcher/studymonitor/StudyParticipantSummaryPage.vue
+++ src/components/researcher/studymonitor/StudyParticipantSummaryPage.vue
@@ -37,7 +37,7 @@
                     </template>
                   </template>
                   <template v-else>
-                    {{ firstLetterUpperCase(props.row.taskType) }}
+                    {{ showTaskName(props.row.taskType) }}
                   </template>
                 </q-td>
               </template>
@@ -236,6 +236,32 @@ export default {
     },
     firstLetterUpperCase (str) {
       return str.charAt(0).toUpperCase() + str.slice(1)
+    },
+    showTaskName (taskType) {
+      switch (taskType) {
+        case 'fingerTapping':
+          return 'Finger Tapping'
+        case 'holdPhone':
+          return 'Hold The Phone'
+        case 'drawing':
+          return this.firstLetterUpperCase(taskType)
+        case 'tugt':
+          return 'Timed up and Go Test'
+        case 'vocalization':
+          return this.firstLetterUpperCase(taskType)
+        case 'peakFlow':
+          return 'Peak Flow'
+        case 'miband':
+          return 'Mi-Band'
+        case 'smwt':
+          return 'Six Minute Walk Test'
+        case 'position':
+          return this.firstLetterUpperCase(taskType)
+        case 'po60':
+          return this.firstLetterUpperCase(taskType)
+        case 'form':
+          return this.firstLetterUpperCase(taskType)
+      }
     },
     async loadTasks (params) {
       this.loading = true
