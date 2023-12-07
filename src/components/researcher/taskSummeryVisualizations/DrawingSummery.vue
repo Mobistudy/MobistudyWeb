488dea859ad23f0441234e6491ac0330794dcfe2
emilia-joha <emilia.joha@live.se>
2023-12-07 15:05:04 +0100

    KAN-83

--- src/components/researcher/taskSummeryVisualizations/DrawingSummery.vue
+++ src/components/researcher/taskSummeryVisualizations/DrawingSummery.vue
@@ -1,11 +1,14 @@
 <template>
-    <div>
-        Drawing Summery
-    </div>
-</template>
+  <div v-if="drawingResult">
+      <div>
+        <canvas id="drawingSummeryChart"></canvas>
+      </div>
+  </div>
+  </template>
 
 <script>
 import API from 'src/shared/API'
+import { Chart } from 'chart.js/auto'
 
 export default {
   props: ['studyKey', 'userKey'],
@@ -14,15 +17,16 @@ export default {
   },
   data () {
     return {
-      taskData: null
+      drawingResult: []
     }
   },
   methods: {
     async fetchTaskData () {
       try {
-        this.taskData = await API.getTasksResults(this.studyKey, this.userKey)
-        const filteredTaskData = this.taskData.filter(task => task.taskType === 'drawing')
-        console.log(filteredTaskData)
+        const taskData = await API.getTasksResults(this.studyKey, this.userKey)
+        const filteredTaskData = taskData.filter(task => task.taskType === 'drawing')
+        this.drawingResult = filteredTaskData
+        this.initializedrawingChart()
       } catch (err) {
         this.$q.notify({
           color: 'negative',
@@ -30,11 +34,61 @@ export default {
           icon: 'report_problem'
         })
       }
+    },
+    getDrawingSummery (drawing, data) {
+      const arr = data.map(result => (Math.round(result.summary[drawing])))
+      return arr
+    },
+    getDrawingSummeryLabels () {
+      const labels = this.drawingResult.map(result => (result.summary.completedTS).slice(0, 10))
+      return labels
+    },
+    initializedrawingChart () {
+      const ctx = document.getElementById('drawingSummeryChart').getContext('2d')
+      const config = {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              data: this.getDrawingSummery('totalVariabilitySquare', this.drawingResult),
+              borderColor: 'blue',
+              backgroundColor: 'blue',
+              label: 'Square'
+            },
+            {
+              data: this.getDrawingSummery('totalVariabilitySpiral', this.drawingResult),
+              borderColor: 'red',
+              backgroundColor: 'red',
+              label: 'Spiral'
+            }
+          ],
+          labels: this.getDrawingSummeryLabels()
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              position: 'bottom',
+              title: {
+                display: true,
+                text: 'Date',
+                color: '#459399'
+              }
+            },
+            y: {
+              type: 'linear',
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Total Variability',
+                color: '#459399'
+              }
+            }
+          }
+        }
+      }
+      this.drawingSummery = new Chart(ctx, config)
     }
   }
 }
 </script>
-
-<style>
-
-</style>
