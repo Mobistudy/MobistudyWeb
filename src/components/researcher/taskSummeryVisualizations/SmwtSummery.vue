bb2729d0ab38d4948de9c2055ee6fda20d4f5272
emilia-joha <emilia.joha@live.se>
2023-12-08 14:06:16 +0100

    KAN-75

--- src/components/researcher/taskSummeryVisualizations/SmwtSummery.vue
+++ src/components/researcher/taskSummeryVisualizations/SmwtSummery.vue
@@ -1,11 +1,20 @@
 <template>
-    <div>
-        Six Minute Walk Test Summery
-    </div>
-</template>
+  <div v-if="smwtResult">
+      <div>
+        <canvas id="smwtDistanceSummeryChart"></canvas>
+      </div>
+      <div>
+        <canvas id="smwtStepsSummeryChart"></canvas>
+      </div>
+      <div>
+        <canvas id="smwtBorgSummeryChart"></canvas>
+      </div>
+  </div>
+  </template>
 
 <script>
 import API from 'src/shared/API'
+import { Chart } from 'chart.js/auto'
 
 export default {
   props: ['studyKey', 'userKey'],
@@ -14,15 +23,18 @@ export default {
   },
   data () {
     return {
-      taskData: null
+      smwtResult: []
     }
   },
   methods: {
     async fetchTaskData () {
       try {
-        this.taskData = await API.getTasksResults(this.studyKey, this.userKey)
-        const filteredTaskData = this.taskData.filter(task => task.taskType === 'smwt')
-        console.log(filteredTaskData)
+        const taskData = await API.getTasksResults(this.studyKey, this.userKey)
+        const filteredTaskData = taskData.filter(task => task.taskType === 'smwt')
+        this.smwtResult = filteredTaskData
+        this.initializesmwtDistanceChart()
+        this.initializesmwtStepsChart()
+        this.initializesmwtBorgChart()
       } catch (err) {
         this.$q.notify({
           color: 'negative',
@@ -30,11 +42,135 @@ export default {
           icon: 'report_problem'
         })
       }
+    },
+    getSmwtSummery (smwt, data) {
+      const arr = data.map(result => (Math.round(result.summary[smwt])))
+      return arr
+    },
+    getSmwtSummeryLabels () {
+      const labels = this.smwtResult.map(result => (result.summary.completedTS).slice(0, 10))
+      return labels
+    },
+    initializesmwtDistanceChart () {
+      const ctx = document.getElementById('smwtDistanceSummeryChart').getContext('2d')
+      const config = {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              data: this.getSmwtSummery('distance', this.smwtResult),
+              borderColor: '#459399',
+              backgroundColor: '#459399',
+              label: 'Distance'
+            }],
+          labels: this.getSmwtSummeryLabels()
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              position: 'bottom',
+              title: {
+                display: true,
+                text: 'Date',
+                color: '#459399'
+              }
+            },
+            y: {
+              type: 'linear',
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Distance',
+                color: '#459399'
+              }
+            }
+          }
+        }
+      }
+      this.smwtDistanceSummery = new Chart(ctx, config)
+    },
+    initializesmwtStepsChart () {
+      const ctx = document.getElementById('smwtStepsSummeryChart').getContext('2d')
+      const config = {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              data: this.getSmwtSummery('steps', this.smwtResult),
+              borderColor: '#459399',
+              backgroundColor: '#459399',
+              label: 'Steps'
+            }],
+          labels: this.getSmwtSummeryLabels()
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              position: 'bottom',
+              title: {
+                display: true,
+                text: 'Date',
+                color: '#459399'
+              }
+            },
+            y: {
+              type: 'linear',
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Steps',
+                color: '#459399'
+              }
+            }
+          }
+        }
+      }
+      this.smwtStepsSummery = new Chart(ctx, config)
+    },
+    initializesmwtBorgChart () {
+      const ctx = document.getElementById('smwtBorgSummeryChart').getContext('2d')
+      const config = {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              data: this.getSmwtSummery('borgScale', this.smwtResult),
+              borderColor: '#459399',
+              backgroundColor: '#459399',
+              label: 'Borg Scale'
+            }],
+          labels: this.getSmwtSummeryLabels()
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              position: 'bottom',
+              title: {
+                display: true,
+                text: 'Date',
+                color: '#459399'
+              }
+            },
+            y: {
+              type: 'linear',
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Borg Scale',
+                color: '#459399'
+              },
+              ticks: {
+                stepSize: 1
+              }
+            }
+          }
+        }
+      }
+      this.smwtBorgSummery = new Chart(ctx, config)
     }
   }
 }
 </script>
-
-<style>
-
-</style>
