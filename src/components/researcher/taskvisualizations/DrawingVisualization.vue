3c554086ea882d7b2e351820712b44b329f1e1c0
simm99 <simm99@live.se>
2023-12-11 16:30:53 +0100

    KAN-66 Update Drawing
    Updated drawing.
    Now the user square and spiral has red colors and are on top of the original figures for easier comparison.

--- src/components/researcher/taskvisualizations/DrawingVisualization.vue
+++ src/components/researcher/taskvisualizations/DrawingVisualization.vue
@@ -1,20 +1,14 @@
 <template>
-  <div>
+  <div class="container">
     <p class="taskVisualizationHeader">Completed: {{ this.niceTimestamp(this.taskProps.row.summary.completedTS) }}</p>
     <div class="figureContainer">
-      <div class="figureSquare">
-        <p>Square</p><canvas class="drawingCanvas" ref="originalSquareCanvas"></canvas>
+      <div class="figureContainerWrapper">
+        <p>Square</p>
+        <canvas class="drawingCanvas" ref="squareCanvas"></canvas>
       </div>
-      <div class="figureSquare">
-        <p>User Square</p><canvas class="drawingCanvas" ref="userSquareCanvas"></canvas>
-      </div>
-    </div>
-    <div class="figureContainer">
-      <div class="figureSpiral">
-        <p>Spiral</p><canvas class="drawingCanvas" ref="originalSpiralCanvas"></canvas>
-      </div>
-      <div class="figureSpiral">
-        <p>User Spiral</p><canvas class="drawingCanvas" ref="userSpiralCanvas"></canvas>
+      <div class="figureContainerWrapper">
+        <p>Spiral</p>
+        <canvas class="drawingCanvas" ref="spiralCanvas"></canvas>
       </div>
     </div>
   </div>
@@ -55,24 +49,36 @@ export default {
       return date.formatDate(timeStamp, 'YYYY-MM-DD HH:mm:ss')
     },
     drawSquare () {
-      const canvas = this.$refs.originalSquareCanvas
+      const canvas = this.$refs.squareCanvas
       const ctx = canvas.getContext('2d')
-      // Hardcoded square coordinates
-      const originalSquare = [{ x: 300, y: 200 }, { x: 300, y: 50 }, { x: 50, y: 50 }, { x: 50, y: 300 }, { x: 300, y: 300 }, { x: 300, y: 200 }]
+      const originalSquare = [
+        { x: 300, y: 200 }, { x: 300, y: 50 }, { x: 50, y: 50 },
+        { x: 50, y: 300 }, { x: 300, y: 300 }, { x: 300, y: 200 }
+      ]
       canvas.width = 350
       canvas.height = 350
       ctx.beginPath()
       ctx.moveTo(originalSquare[0].x, originalSquare[0].y)
+      ctx.strokeStyle = 'black'
+      ctx.lineWidth = 2
       for (const point of originalSquare) {
         ctx.lineTo(point.x, point.y)
       }
       ctx.closePath()
       ctx.stroke()
+      const userSquareCoordinates = this.data.square.touchCoordinates
+      ctx.beginPath()
+      ctx.moveTo(userSquareCoordinates[0].x, userSquareCoordinates[0].y)
+      ctx.strokeStyle = 'red'
+      ctx.lineWidth = 2
+      for (const point of userSquareCoordinates) {
+        ctx.lineTo(point.x, point.y)
+      }
+      ctx.stroke()
     },
     drawSpiral () {
-      const canvas = this.$refs.originalSpiralCanvas
+      const canvas = this.$refs.spiralCanvas
       const ctx = canvas.getContext('2d')
-      // Hardcoded spiral coordinates
       const originalSpiral = [
         { x: 300, y: 200 }, { x: 300, y: 50 }, { x: 50, y: 50 }, { x: 50, y: 300 }, { x: 250, y: 300 }, { x: 250, y: 100 },
         { x: 100, y: 100 }, { x: 100, y: 250 }, { x: 200, y: 250 }, { x: 200, y: 150 }, { x: 150, y: 150 }, { x: 150, y: 200 }
@@ -81,7 +87,19 @@ export default {
       canvas.height = 350
       ctx.beginPath()
       ctx.moveTo(originalSpiral[0].x, originalSpiral[0].y)
+      ctx.strokeStyle = 'black'
+      ctx.lineWidth = 2
       for (const point of originalSpiral) {
+        ctx.lineTo(point.x, point.y)
+      }
+      ctx.stroke()
+
+      const userSpiralCoordinates = this.data.spiral.touchCoordinates
+      ctx.beginPath()
+      ctx.moveTo(userSpiralCoordinates[0].x, userSpiralCoordinates[0].y)
+      ctx.strokeStyle = 'red'
+      ctx.lineWidth = 2
+      for (const point of userSpiralCoordinates) {
         ctx.lineTo(point.x, point.y)
       }
       ctx.stroke()
@@ -133,25 +151,29 @@ export default {
 </script>
 
 <style>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
 .taskVisualizationHeader {
+  width: 100%;
   text-align: center;
   font-weight: bold;
   color: #459399;
 }
 .figureContainer {
+  width: 100%;
   display: flex;
   justify-content: space-between;
   margin-bottom: 20px;
 }
-.figureSquare, .figureSpiral {
-  width: 50%;
-  box-sizing: border-box;
-}
+
 .drawingCanvas {
   width: 100%;
   height: 100%;
 }
-.figureSquare p, .figureSpiral p {
+.figureContainer p {
   text-align: center;
   font-size: 16px;
   font-weight: bold;
